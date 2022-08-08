56272f4fe610e416e6b5938131e98f1d093f4b05
Bryce Wein <bryce.wein@gmail.com>
2022-08-08 13:04:32 -0400

    Test: finalize testing

--- spec/poros/resturant_spec.rb
+++ spec/poros/resturant_spec.rb
@@ -8,4 +8,14 @@ RSpec.describe Resturant, :vcr do
     expect(resturant.name).to be_a String
     expect(resturant.address).to be_a String
   end
+
+  it '#create_address' do
+    parsed_json = YelpService.get_resturants('chinese', 'denver, co')
+
+    resturant = YelpFacade.create_resturant('chinese', 'denver, co')
+
+    address = resturant.format_address(parsed_json[:businesses][0])
+
+    expect(address).to be_a String
+  end
 end
