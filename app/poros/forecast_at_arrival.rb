cd566bf635848219fab8adca8c73986b28ef09b8
Bryce Wein <bryce.wein@gmail.com>
2022-08-08 14:15:38 -0400

    Refactor: account for trips over 2 days in length

--- app/poros/forecast_at_arrival.rb
+++ app/poros/forecast_at_arrival.rb
@@ -1,6 +1,22 @@
 class ForecastAtArrival
   def initialize(data, hours_to_destination)
-    @temperature = data[:hourly][hours_to_destination][:temp]
-    @conditions = data[:hourly][hours_to_destination][:weather][0][:description]
+    @temperature = set_temp(data, hours_to_destination)
+    @conditions = set_conditions(data, hours_to_destination)
+  end
+
+  def set_temp(data, hours_to_destination)
+    if hours_to_destination > 48
+      data[:daily][(hours_to_destination / 24).to_i][:temp][:day]
+    else
+      data[:hourly][hours_to_destination][:temp]
+    end
+  end
+
+  def set_conditions(data, hours_to_destination)
+    if hours_to_destination > 48
+      data[:daily][(hours_to_destination / 24).to_i][:weather][0][:description]
+    else
+      data[:hourly][hours_to_destination][:weather][0][:description]
+    end
   end
 end
