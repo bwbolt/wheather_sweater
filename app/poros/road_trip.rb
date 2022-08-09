b47d7e1927bd67cfaba7192ac695501e46d8aa5d
Bryce Wein <bryce.wein@gmail.com>
2022-08-09 14:35:20 -0400

    Refactor: move all service calls out of roadtrip poro

--- app/poros/road_trip.rb
+++ app/poros/road_trip.rb
@@ -1,11 +1,11 @@
 class RoadTrip
   attr_reader :id, :start_city, :end_city, :travel_time, :weather_at_eta
 
-  def initialize(origin, destination)
+  def initialize(origin, destination, travel_time, weather_at_eta)
     @id = nil
     @start_city = origin
     @end_city = destination
-    @travel_time = MapquestFacade.get_travel_time(origin, destination)
-    @weather_at_eta = OpenweatherFacade.create_weather_at_eta(destination, @travel_time)
+    @travel_time = travel_time
+    @weather_at_eta = weather_at_eta
   end
 end
--- spec/poros/road_trip_spec.rb
+++ spec/poros/road_trip_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 RSpec.describe RoadTrip, :vcr do
   it 'exists with attributes' do
-    road_trip = RoadTrip.new('denver,co', 'New York, NY')
+    weather_at_eta = {
+      "temperature": 78.37,
+      "conditions": 'scattered clouds'
+    }
+
+    road_trip = RoadTrip.new('denver,co', 'New York, NY', '4', weather_at_eta)
 
     expect(road_trip).to be_a RoadTrip
 
@@ -12,11 +17,14 @@ RSpec.describe RoadTrip, :vcr do
     expect(road_trip.end_city).to be_a String
     expect(road_trip.end_city).to eq 'New York, NY'
     expect(road_trip.travel_time).to be_a String
-    expect(road_trip.weather_at_eta).to be_a ForecastAtArrival
+    expect(road_trip.weather_at_eta).to be_a Hash
   end
 
   it 'returns even with impossible route' do
-    road_trip = RoadTrip.new('denver,co', 'Hawaii')
+    weather_at_eta = nil
+    travel_time = 'impossible route'
+
+    road_trip = RoadTrip.new('denver,co', 'Hawaii', travel_time, weather_at_eta)
 
     expect(road_trip).to be_a RoadTrip
 
