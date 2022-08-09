3bbe3ec1ec7bc13a824173d2bcce61655ed6975b
Bryce Wein <bryce.wein@gmail.com>
2022-08-09 11:05:51 -0400

    refactor: update roadtrip spec for impossible route

--- spec/poros/road_trip_spec.rb
+++ spec/poros/road_trip_spec.rb
@@ -14,4 +14,19 @@ RSpec.describe RoadTrip, :vcr do
     expect(road_trip.travel_time).to be_a String
     expect(road_trip.weather_at_eta).to be_a ForecastAtArrival
   end
+
+  it 'returns even with impossible route' do
+    road_trip = RoadTrip.new('denver,co', 'Hawaii')
+
+    expect(road_trip).to be_a RoadTrip
+
+    expect(road_trip.id).to eq nil
+    expect(road_trip.start_city).to be_a String
+    expect(road_trip.start_city).to eq 'denver,co'
+    expect(road_trip.end_city).to be_a String
+    expect(road_trip.end_city).to eq 'Hawaii'
+    expect(road_trip.travel_time).to be_a String
+    expect(road_trip.travel_time).to eq 'impossible route'
+    expect(road_trip.weather_at_eta).to eq nil
+  end
 end
